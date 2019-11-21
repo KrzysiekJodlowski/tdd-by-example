50b24fb8ac5df3b58139ede04db238afec291591
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-11-21 12:26:43 +0100

    Chapter 14 - exchange

--- src/main/java/com/jodlowski/WyCash/Bank.java
+++ src/main/java/com/jodlowski/WyCash/Bank.java
@@ -1,8 +1,40 @@
 package com.jodlowski.WyCash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
 
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        return rates.get(new Pair(from, to));
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public boolean equals(Object object) {
+            Pair pair = (Pair) object;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        public int hashCode() {
+            return 0;
+        }
     }
 }
