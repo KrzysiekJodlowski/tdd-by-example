5a30d2a6c5de60f27533f231155bf9893f729d3b
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-11-25 19:18:18 +0100

    Chapter 23 - unity is strength

--- part_2/test_case_test.py
+++ part_2/test_case_test.py
@@ -1,33 +1,47 @@
 from test_case import TestCase
 from was_run import WasRun
 from test_result import TestResult
+from test_suite import TestSuite
 
 
 class TestCaseTest(TestCase):
+    def setup(self):
+        self.result = TestResult()
 
     def test_template_method(self):
         test = WasRun("test_method")
-        test.run()
+        test.run(self.result)
         assert ("setUp testMethod tearDown " == test.log)
 
     def test_result(self):
         test = WasRun("test_method")
-        result = test.run()
-        assert ("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert ("1 run, 0 failed" == self.result.summary())
 
     def test_failed_result(self):
         test = WasRun("test_broken_method")
-        result = test.run()
-        assert ("1 run, 1 failed" == result.summary)
+        test.run(self.result)
+        assert ("1 run, 1 failed" == self.result.summary())
 
     def test_failed_result_formatting(self):
-        result = TestResult()
-        result.test_started()
-        result.test_failed()
-        assert ("1 run, 1 failed" == result.summary())
+        self.result.test_started()
+        self.result.test_failed()
+        assert ("1 run, 1 failed" == self.result.summary())
+
+    def test_suite(self):
+        suite = TestSuite()
+        suite.add(WasRun("test_method"))
+        suite.add(WasRun("test_broken_method"))
+        suite.run(self.result)
+        assert ("2 run, 1 failed" == self.result.summary())
 
 
-TestCaseTest("test_template_method").run()
-TestCaseTest("test_result").run()
-TestCaseTest("test_failed_result").run()
-TestCaseTest("test_failed_result_formatting").run()
+suite = TestSuite()
+suite.add(TestCaseTest("test_template_method"))
+suite.add(TestCaseTest("test_result"))
+suite.add(TestCaseTest("test_failed_result_formatting"))
+suite.add(TestCaseTest("test_failed_result"))
+suite.add(TestCaseTest("test_suite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
