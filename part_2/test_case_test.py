042ac6e8a61472203bef73fa14033d1c9b511202
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-11-25 11:42:59 +0100

    Chapter 19 - everybody..

--- part_2/test_case_test.py
+++ part_2/test_case_test.py
@@ -4,11 +4,17 @@ from was_run import WasRun
 
 class TestCaseTest(TestCase):
 
+    def setup(self):
+        self.test = WasRun("test_method")
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert not test.was_run
-        test.run()
-        assert test.was_run
+        self.test.run()
+        assert self.test.was_run
+
+    def test_setup(self):
+        self.test.run()
+        assert self.test.was_setup
 
 
 TestCaseTest("test_running").run()
+TestCaseTest("test_setup").run()
--- part_2/was_run.py
+++ part_2/was_run.py
@@ -3,8 +3,11 @@ from test_case import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.was_run = None
         TestCase.__init__(self, name)
 
     def test_method(self):
         self.was_run = 1
+
+    def setup(self):
+        self.was_run = None
+        self.was_setup = 1
